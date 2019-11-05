4cb564a2b674f7190b6854ce5793f21be2af8e21
George Rosenberger <gr2578@cumc.columbia.edu>
2019-11-05 09:10:47 -0500

    [RELEASE] Version 1.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='secat',
-    version='1.0.1',
+    version='1.0.2',
     description='Size-Exclusion Chromatography Algorithmic Toolkit',
     long_description=long_description,
     long_description_content_type='text/markdown',
